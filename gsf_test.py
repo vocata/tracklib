fd2570c9ab8766dbdf0a61a1df6008af4f0df584
ray <leixueyue@gmail.com>
2020-04-09 20:21:40 +0800

    add gsf_test

---
+++ gsf_test.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy.linalg as lg
+import tracklib as tlb
+import tracklib.filter as ft
+import tracklib.model as model
+import matplotlib.pyplot as plt
+'''
+notes:
+vector is preferably a column vector, otherwise
+the program may yield uncertain result.
+'''
+
+
+def GSFilter_test():
+    '''
+    Gaussian sum filter test program
+    '''
+    N, T = 200, 1
+    model_n = 3
+
+    x_dim, z_dim = 4, 2
+    gsf = ft.MMFilter()
+    for i in range(model_n):
+        # qx, qy = np.sqrt((i + 1) / 100), np.sqrt((i + 1) / 100)
+        # rr, ra = np.sqrt((i + 1) / 10), np.sqrt((i + 1) / 100)
+        qx, qy = np.sqrt(0.01), np.sqrt(0.01)
+        rr, ra = np.sqrt(0.1), np.sqrt(0.01)
+
+        F = model.F_poly_trans(1, 1, T)
+        f = lambda x, u: F @ x
+        L = np.eye(x_dim)
+        Q = model.Q_dd_poly_proc_noise(1, 1, T, [qx, qy])
+
+        h = lambda x: np.array([lg.norm(x[0: 2]), np.arctan2(x[1], x[0])])
+        M = np.eye(z_dim)
+        R = model.R_only_pos_meas_noise(1, [rr, ra])
+
+        # sub_filter = ft.KFilter(F, L, H, M, Q, R)
+        sub_filter = ft.EKFilterAN(f, L, h, M, Q, R, order=1, it=0)
+        gsf.add_model(sub_filter, 1 / model_n)
+
+    # initial state and error convariance
+    x = np.array([1, 2, 0.2, 0.3])
+    P = []
+    P.append(10 * np.eye(x_dim))
+    P.append(15 * np.eye(x_dim))
+    P.append(20 * np.eye(x_dim))
+
+    gsf.init(x, P)
+
+    state_arr = np.empty((x_dim, N))
+    measure_arr = np.empty((z_dim, N))
+    weight_state_arr = np.empty((x_dim, N))
+    maxprob_state_arr = np.empty((x_dim, N))
+    prob_arr = np.empty((model_n, N))
+
+    for n in range(N):
+        w = tlb.multi_normal(0, Q)
+        v = tlb.multi_normal(0, R)
+
+        x = f(x, 0) + L @ w
+        z = h(x) + M @ v
+        state_arr[:, n] = x
+        measure_arr[:, n] = tlb.pol2cart(z[0], z[1])
+        gsf.step(z)
+        
+        weight_state_arr[:, n] = gsf.weighted_state
+        maxprob_state_arr[:, n] = gsf.maxprob_state
+        prob_arr[:, n] = gsf.prob
+    print(len(gsf))
+    print(gsf)
+
+    # plot
+    n = np.arange(N)
+    _, ax = plt.subplots(2, 1)
+    ax[0].plot(n, state_arr[0, :], linewidth=0.8)
+    ax[0].plot(n, measure_arr[0, :], '.')
+    ax[0].plot(n, weight_state_arr[0, :], linewidth=0.8)
+    ax[0].plot(n, maxprob_state_arr[0, :], linewidth=0.8)
+    ax[0].legend(['real', 'measurement', 'weighted esti', 'max prob esti'])
+    ax[0].set_title('x state')
+    ax[1].plot(n, state_arr[1, :], linewidth=0.8)
+    ax[1].plot(n, measure_arr[1, :], '.')
+    ax[1].plot(n, weight_state_arr[1, :], linewidth=0.8)
+    ax[1].plot(n, maxprob_state_arr[1, :], linewidth=0.8)
+    ax[1].legend(['real', 'measurement', 'weighted esti', 'max prob esti'])
+    ax[1].set_title('y state weighted estimation')
+    plt.show()
+
+    _, ax = plt.subplots()
+    for i in range(model_n):
+        ax.plot(n, prob_arr[i, :])
+    ax.set_xlabel('time(s)')
+    ax.set_ylabel('probability')
+    ax.legend([str(n) for n in range(model_n)])
+    plt.show()
+
+    # trajectory
+    _, ax = plt.subplots()
+    ax.scatter(state_arr[0, 0], state_arr[1, 0], s=120, c='r', marker='x')
+    ax.plot(state_arr[0, :], state_arr[1, :], linewidth=0.8)
+    ax.plot(measure_arr[0, :], measure_arr[1, :], linewidth=0.8)
+    ax.plot(weight_state_arr[0, :], weight_state_arr[1, :], linewidth=0.8)
+    ax.plot(maxprob_state_arr[0, :], maxprob_state_arr[1, :], linewidth=0.8)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.legend(['real', 'measurement', 'weighted esti', 'max prob esti'])
+    ax.set_title('trajectory')
+    plt.show()
+
+
+if __name__ == '__main__':
+    GSFilter_test()
--- tracklib/filter/mmf.py
+++ tracklib/filter/mmf.py
@@ -3,7 +3,7 @@
 The multiple model filter can use other types of Kalman filters as its submodels
 for filtering. Currently supported filters are stardard Kalman filter(KFilter),
 extended Kalman filter and unscented Kalman filter. For the non-linear system with
-additive Gaussian noise, this multiple model filter can be viewed as Gaussian sum
+additive Gaussian noise, this multiple model filter can be used as Gaussian sum
 filter by setting different initial state and convariance of each non-linear filters
 or submodels.
 '''
@@ -22,7 +22,7 @@ from .ukf import UKFilterAN, UKFilterNAN
 
 class MMFilter(KFBase):
     '''
-    Hybird multiple model filter
+    Hybrid multiple model filter
     '''
     def __init__(self):
         super().__init__()
@@ -30,7 +30,7 @@ class MMFilter(KFBase):
         self._model_n = 0
 
     def __str__(self):
-        msg = 'Multiple model filter:\n{\n  '
+        msg = 'Hybrid multiple model filter:\n{\n  '
         if self._model_n < 10:
             sub = ['{}: model: {}, probability: {}'.format(i, self._model[i][0], self._model[i][1]) for i in range(self._model_n)]
             sub = '\n  '.join(sub)
@@ -59,30 +59,30 @@ class MMFilter(KFBase):
 
         Parameters
         ----------
-        state : ndarray or list or None
+        state : ndarray or list
             Initial prior state estimate
-        cov : ndarray or list or None
+        cov : ndarray or list
             Initial error convariance matrix
-
-        Note : if state or cov is None, the model has been initialized outside 
 
         Returns
         -------
             None
         '''
-        if state is None and cov is None:
+        if isinstance(state, list):
             pass
+        elif isinstance(state, np.ndarray):
+            state = [state] * self._model_n
         else:
-            if isinstance(state, np.ndarray) and isinstance(cov, np.ndarray):
-                for i in range(self._model_n):
-                    self._model[i][0].init(state, cov)
-            elif isinstance(state, list) and isinstance(cov, list):
-                for i in range(self._model_n):
-                    if state[i] is None or cov[i] is None:
-                        continue
-                    self._model[i][0].init(state[i], cov[i])
-            else:
-                raise ValueError('state and cov must be a ndarray, list or None')
+            raise ValueError('state must be a ndarray, list, not %s' % state.__class__.__name__)
+        if isinstance(cov, list):
+            pass
+        elif isinstance(cov, np.ndarray):
+            cov = [cov] * self._model_n
+        else:
+            raise ValueError('cov must be a ndarray, list, not %s' % cov.__class__.__name__)
+
+        for i in range(self._model_n):
+            self._model[i][0].init(state[i], cov[i])
         self._len = 0
         self._stage = 0
         self._init = True
@@ -106,28 +106,29 @@ class MMFilter(KFBase):
             self._model[self._model_n] = [model, prob]
             self._model_n += 1
 
-    def predict(self, u=None):
+    def predict(self, u=None, **kw):
         assert (self._stage == 0)
         if self._init == False:
             raise RuntimeError('the filter must be initialized with init() before use')
 
         for i in range(self._model_n):
-            self._model[i][0].predict(u)
+            self._model[i][0].predict(u, **kw)
 
         self._stage = 1
 
-    def update(self, z):
+    def update(self, z, **kw):
         assert (self._stage == 1)
         if self._init == False:
             raise RuntimeError('the filter must be initialized with init() before use')
 
         pdf = []
         for i in range(self._model_n):
-            self._model[i][0].update(z)
+            self._model[i][0].update(z, **kw)
             r = self._model[i][0].innov
             S = self._model[i][0].innov_cov
-            pdf.append((np.exp(-r.T @ lg.inv(S) @ r / 2) /
-                        np.sqrt(lg.det(2 * np.pi * S))).item())
+            # If there is a wild value, exp will be very small and all values in the pdf will be 0,
+            # then total defined below will be 0 and an ZeroDivisionError will occur.
+            pdf.append((np.exp(-r @ lg.inv(S) @ r / 2) / np.sqrt(lg.det(2 * np.pi * S))).item())
 
         # total probability
         total = 0
@@ -140,11 +141,11 @@ class MMFilter(KFBase):
         self._len += 1
         self._stage = 0
 
-    def step(self, z, u=None):
+    def step(self, z, u=None, **kw):
         assert (self._stage == 0)
 
-        self.predict(u)
-        self.update(z)
+        self.predict(u, **kw)
+        self.update(z, **kw)
         
     @property
     def weighted_state(self):
