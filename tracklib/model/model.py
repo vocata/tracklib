0d200a198144e8731b8c2803e265ebd53d43d14f
ray <leixueyue@gmail.com>
2020-05-04 20:22:42 +0800

    add singer model

--- tracklib/model/model.py
+++ tracklib/model/model.py
@@ -7,10 +7,10 @@ from __future__ import division, absolute_import, print_function
 
 
 __all__ = [
-    'F_poly', 'Q_poly_dc', 'Q_poly_dd', 'H_pos_only', 'R_pos_only', 'F_cv',
-    'Q_cv_dc', 'Q_cv_dd', 'H_cv', 'R_cv', 'F_ca', 'Q_ca_dc', 'Q_ca_dd', 'H_ca',
-    'R_ca', 'F_ct2D', 'f_ct2D', 'f_ct2D_jac', 'Q_ct2D', 'h_ct2D', 'h_ct2D_jac',
-    'R_ct2D', 'model_switch', 'Trajectory2D'
+    'F_poly', 'F_singer', 'Q_poly_dc', 'Q_poly_dd', 'Q_singer', 'H_pos_only',
+    'R_pos_only', 'F_cv', 'Q_cv_dc', 'Q_cv_dd', 'H_cv', 'R_cv', 'F_ca',
+    'Q_ca_dc', 'Q_ca_dd', 'H_ca', 'R_ca', 'F_ct2D', 'f_ct2D', 'f_ct2D_jac',
+    'Q_ct2D', 'h_ct2D', 'h_ct2D_jac', 'R_ct2D', 'model_switch', 'Trajectory2D'
 ]
 
 import numpy as np
@@ -24,7 +24,7 @@ from scipy.special import factorial
 def F_poly(order, axis, T):
     '''
     This polynomial transition matrix is used with discretized continuous-time
-    models as well as direct discrete-time models. see [1] section 6.2 and 6.3.
+    models as well as direct discrete-time models. see section 6.2 and 6.3 in [1].
 
     Parameters
     ----------
@@ -56,10 +56,49 @@ def F_poly(order, axis, T):
     return F
 
 
+def F_singer(axis, T, tau=20):
+    '''
+    Get the singer model transition matrix, see section 8.2 in [1].
+
+    Parameters
+    ----------
+    axis : int
+        Motion directions in Cartesian coordinate. If axis=1, it means x-axis,
+        2 means x-axis and y-axis, etc.
+    T : float
+        The time-duration of the propagation interval.
+    tau : float
+        The time constant of the target acceleration autocorrelation, that is, the
+        decorrelation time is approximately 2*tau. A reasonable range of tau for
+        Singer's model is between 5 and 20 seconds. Typical values of tua for aircraft
+        are 20s for slow turn and 5s for an evasive maneuver. If this parameter
+        is omitted, the default value of 20 is used.The time constant is assumed
+        the same for all dimensions of motion, so this parameter is scalar.
+    Returns
+    -------
+    F : ndarray
+        The state transition matrix under a Gauss-Markov dynamic model of the given
+        axis.
+    '''
+    assert (axis >= 1)
+
+    alpha = 1 / tau
+    F_base = np.zeros((3, 3))
+    F[0, 0] = 1
+    F[0, 1] = T
+    F[0, 2] = (alpha * T - 1 + np.exp(-alpha * T)) * tau**2
+    F[1, 1] = 1
+    F[1, 2] = (1 - np.exp(-alpha * T)) * tau
+    F[2, 2] = np.exp(-alpha * T)
+    F = np.kron(np.eye(axis), F_base)
+
+    return F
+
+
 def Q_poly_dc(order, axis, T, std):
     '''
     Process noise covariance matrix used with discretized continuous-time models.
-    see [1] section 6.2.
+    see section 6.2 in [1].
 
     Parameters
     ----------
@@ -71,7 +110,7 @@ def Q_poly_dc(order, axis, T, std):
         2 means x-axis and y-axis, etc.
     T : float
         The time-duration of the propagation interval.
-    std : number, list or ndarray
+    std : number, list
         The standard deviation (square root of intensity) of continuous-time porcess noise
 
     Returns
@@ -95,7 +134,7 @@ def Q_poly_dc(order, axis, T, std):
 def Q_poly_dd(order, axis, T, std, ht=0):
     '''
     Process noise covariance matrix used with direct discrete-time models.
-    see [1] section 6.3.
+    see section 6.3 in [1].
 
     Parameters
     ----------
@@ -107,15 +146,15 @@ def Q_poly_dd(order, axis, T, std, ht=0):
         2 means x-axis and y-axis, etc.
     T : float
         The time-duration of the propagation interval.
-    std : number, list or ndarray
+    std : number, list
         The standard deviation of discrete-time porcess noise
     ht : int
         ht means that the order of the noise is 'ht' greater than the highest order
         of the state, e.g., if the highest order of state is acceleration, then ht=0
         means that the noise is of the same order as the highest order of state, that
-        is, the noise is acceleration and the model is DWPA, see[1] section 6.3.3. If
-        the highest order is velocity, the ht=1 means the noise is acceleration and
-        the model is DWNA, see[1] section 6.3.2.
+        is, the noise is acceleration and the model is DWPA, see section 6.3.3 in [1].
+        If the highest order is velocity, the ht=1 means the noise is acceleration and
+        the model is DWNA, see section 6.3.2 in [1].
 
     Returns
     -------
@@ -141,10 +180,65 @@ def Q_poly_dd(order, axis, T, std, ht=0):
     return Q
 
 
+def Q_singer(axis, T, tau, std):
+    '''
+    Process noise covariance matrix used with Singer models. see section 8.2 in [1]
+
+    Parameters
+    ----------
+    axis : int
+        Motion directions in Cartesian coordinate. If axis=1, it means x-axis,
+        2 means x-axis and y-axis, etc.
+    T : float
+        The time-duration of the propagation interval.
+    tau : float
+        The time constant of the target acceleration autocorrelation, that is, the
+        decorrelation time is approximately 2*tau. A reasonable range of tau for
+        Singer's model is between 5 and 20 seconds. Typical values of tua for aircraft
+        are 20s for slow turn and 5s for an evasive maneuver. If this parameter
+        is omitted, the default value of 20 is used.The time constant is assumed
+        the same for all dimensions of motion, so this parameter is scalar.
+    std : number, list
+        std^2 is the instantaneous variance of the acceleration, which can be obtained
+        by assuming it to be
+        1. Equal to a maxmum acceleration a_M with probability p_M and -a_M with the same
+           probability
+        2. Equal to zero with probability p_0
+        3. Uniformly distributed in [-a_M, a_M] with the remaining probability mass
+        All parameters mentioned above are chosen by the designer. So the expected std^2
+        is (a_M^2 / 3)*(1 + 4*p_M - p_0)
+
+    Returns
+    -------
+    Q : ndarray
+        Process noise convariance
+    '''
+    assert (axis >= 1)
+
+    if isinstance(std, (int, float)):
+        std = [std] * axis
+    alpha = 1 / tau
+    aT = alpha * T
+    eaT = np.exp(-aT)
+    e2aT = np.exp(-2 * aT)
+
+    q11 = tau**4 * (1 - e2aT + 2 * aT + 2 * aT**3 / 3 - 2 * aT**2 - 4 * aT * eaT)
+    q12 = tau**3 * (e2aT + 1 - 2 * eaT + 2 * aT * eaT - 2 * aT + aT**2)
+    q13 = tau**2 * (1 - e2aT - 2 * aT * eaT)
+    q22 = tau**2 * (4 * eaT - 3 - e2aT + 2 * aT)
+    q23 = tau * (e2aT + 1 - 2 * eaT)
+    q33 = 1 - e2aT
+    Q_base = np.array([[q11, q12, q13],
+                       [q12, q22, q23],
+                       [q13, q23, q33]], dtype=float)
+    Q = np.kron(np.diag(std)**2, Q_base)
+
+    return Q
+
 def H_pos_only(order, axis):
     '''
     Position-only measurement matrix is used with discretized continuous-time models
-    as well as direct discrete-time models. see [1] section 6.5.
+    as well as direct discrete-time models. see section 6.5 in [1].
 
     Parameters
     ----------
